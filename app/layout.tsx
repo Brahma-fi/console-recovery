0db179cc66ee6cf9e1fdc7302d579f3f724353a2
arunshekhar <arunshekhar001@gmail.com>
2025-11-05 17:45:50 +0530

    og up 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export const metadata: Metadata = {
     description: APP_CONFIG.description,
     images: [
       {
-        url: "/images/Recovery.png",
+        url: "/images/OG.png",
         width: 1200,
         height: 630,
         alt: "Brahma Account Recovery",
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: APP_CONFIG.name,
     description: APP_CONFIG.description,
-    images: ["/images/Recovery.png"],
+    images: ["/images/OG.png"],
   },
 };
 
